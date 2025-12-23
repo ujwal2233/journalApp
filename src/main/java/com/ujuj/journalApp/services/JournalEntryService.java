1ee569c77e10ce0ea80d9eec805cc7b7065cfec8
ujwal2233 <your-email@example.com>
2025-12-23 23:43:55 +0530

    Updates

--- src/main/java/com/ujuj/journalApp/services/JournalEntryService.java
+++ src/main/java/com/ujuj/journalApp/services/JournalEntryService.java
@@ -28,7 +28,6 @@ public class JournalEntryService {
             journalEntry.setDate(LocalDateTime.now());
             JournalEntry saved = journalEntryRepository.save(journalEntry);
             user.getJournalEntries().add(saved);
-            user.setUserName(null);
             userService.saveEntry(user);
         }catch (Exception e)
         {
